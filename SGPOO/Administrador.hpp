116ce6a240a73f5af89d248faed3f216966e5e22
Robson Delas <robsonbielawski@yahoo.com.br>
2021-06-19 15:19:35 -0300

    *adicionando classe administrador

---
+++ SGPOO/Administrador.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef Administrador_hpp
+#define Administrador_hpp
+#include "Empregado.hpp"
+
+class Administrador : public Empregado 
+{
+public:
+	Administrador(string nome, string endereco, string telefone, int codigoSetor, float salario, float ajudaDeCusto);
+	Administrador();
+	float calculaSalario();
+	void imprimirDetalhes();
+
+private:
+	float ajudaDeCusto;
+};
+#endif //!Administrador_hpp
