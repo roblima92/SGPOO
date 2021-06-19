8f5274b9228b18749b24ceede509968c0e3919cd
Robson Delas <robsonbielawski@yahoo.com.br>
2021-06-19 15:20:45 -0300

    *corrigindo logica calcula salario e transformando calculaSalario para virtual puro

--- SGPOO/Empregado.cpp
+++ SGPOO/Empregado.cpp
@@ -25,27 +25,32 @@ void Empregado::setSalarioBase(float salario)
 	this->salario = salario;
 }
 
-float Empregado::calculaSalario()
+/*float Empregado::calculaSalario() {
+	//NADA AQUI. CADA CLASSE VAI REIMPLEMENTAR O METODO calculaSalario DA PARENT CLASS EMPREGADO. 
+	//PORTANTO, calculaSalario NÃO PRECISA SER IMPLEMENTADO NA CLASSE EMPREGADO.
+}*/
+
+float Empregado::descontoImposto() 
 {
+	//METODO QUE CALCULA O IMPOSTO A SER DESCONTADO DO SALARIO;
 	if (salario < 1903.99) {
-		return salario;
-	}else if (salario>= 1903.99 && salario<= 2826.65) {
-		return salario - (salario * 7, 5) / 100;
+		return 0;
+	}
+	else if (salario >= 1903.99 && salario <= 2826.65) {
+		return (salario * 7, 5) / 100;
 	}
 	else if (salario >= 2826.66 && salario <= 3751.05) {
-		return salario - (salario * 15) / 100;
+		return (salario * 15) / 100;
 	}
-	else if(salario >= 3751.06 && salario <= 4664.68){
-		return salario - (salario * 22, 5) / 100;
+	else if (salario >= 3751.06 && salario <= 4664.68) {
+		return (salario * 22, 5) / 100;
 	}
-	else
-	{
-		return salario - (salario * 27, 5) / 100;
+	else {
+		return (salario * 27, 5) / 100;
 	}
 }
 
 void Empregado::imprimirDetalhes()
 {
 	Pessoa::imprimirDetalhes();
-	cout << "Tem o salario LIQUIDO de: " << this->calculaSalario() << endl;
 }
--- SGPOO/Empregado.hpp
+++ SGPOO/Empregado.hpp
@@ -3,14 +3,15 @@
 #define Empregado_hpp
 #include "Pessoa.hpp"
 
-class Empregado : public Pessoa {
-
+class Empregado : public Pessoa 
+{
 public:
 	Empregado();
 	Empregado(string nome, string endereço, string telefone, int codigoSetor, float salario);
 	void setCodigoSetor(int credito);
 	void setSalarioBase(float salario);
-	virtual float calculaSalario();
+	virtual float calculaSalario() = 0;			//METODO VIRTUAL, POIS CADA CLASSE QUE HERDA DE EMPREGADO, TEM UM FORMULA DIFERENTE PARA CALCULO DO SEU SÁLARIO;
+	float descontoImposto();
 	void imprimirDetalhes();
 
 protected:
@@ -18,4 +19,3 @@ protected:
 	float salario;
 };
 #endif //!Empregado_hpp
-
